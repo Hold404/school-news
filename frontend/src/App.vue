98b50c0dd69d010de5ca9fdecd0e46a9114777ab
Maxim Sergeev <eg.dor12@yandex.ru>
2023-01-12 22:24:35 +0200

    frontend update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,11 @@
 <template>
-  <RouterView />
+  <div class="wrapper">
+    <Header />
+    <RouterView />
+  </div>
 </template>
+
+<script setup lang="ts">
+
+import Header from "@/components/Header.vue";
+</script>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <main class="home">
+
+  </main>
+</template>
+
+<script setup lang="ts">
+
+</script>
