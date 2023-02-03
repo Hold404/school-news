8a6dbcaceaff6f356fef91c6371908741127e71a
Egor Dorofeev <68229432+Hold404@users.noreply.github.com>
2023-02-03 12:16:10 +0200

    Add files via upload

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="wrapper">
-    <Header />
-    <RouterView />
+    <Header class="header" />
+    <RouterView class="content" />
+    <Footer class="footer" />
   </div>
 </template>
 
 <script setup lang="ts">
-
 import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 </script>
+
+<style scoped lang="scss">
+.wrapper {
+  display: flex;
+  flex-direction: column;
+
+  .content {
+    flex: 1 1 auto;
+  }
+}
+</style>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "@/views/HomeView.vue";
+import RegisterView from '@/views/RegisterView.vueView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/signup',
+      name: 'signup',
+      component: RegisterView
     }
   ],
 });
